57962f2fab1fd85e75c24b4e0a864a8256996954
JavohirMX <javohirmx@gmail.com>
2025-03-10 03:11:05 +0900

    Retrieve location data based on client IP for prayer times display

--- prayer_times/views.py
+++ prayer_times/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from .utils import get_prayer_times
+from utils.location import get_client_ip, get_ip_location
 # Create your views here.
 
 def prayer_times(request):
-    data = get_prayer_times('Seoul', 'South Korea')
+    ip = get_client_ip(request)
+    location = get_ip_location(ip)
+    data = get_prayer_times(location.get("city","Seoul"), location.get("country","South Korea"))
     context = {
         'data': data["data"],
+        'location': location,
     }
     return render(request, 'prayer_times/prayer_times.html', context)
--- utils/location.py
+++ utils/location.py
@@ -7,6 +7,14 @@ def get_client_ip(request):
         ip = x_forwarded_for.split(',')[0]
     else:
         ip = request.META.get('REMOTE_ADDR')
+        
+     # If running locally, get public IP
+    if ip in ["127.0.0.1", "localhost"]:
+        try:
+            ip = requests.get("https://api64.ipify.org?format=json").json().get("ip")
+        except requests.RequestException:
+            ip = "UNKNOWN"
+            
     return ip
 
 def get_ip_location(ip):
@@ -27,3 +35,9 @@ def get_ip_location(ip):
     except requests.RequestException:
         return {"error": "Could not retrieve location data"}
     return {"error": "Invalid IP or failed lookup"}
+
+if __name__ == '__main__':
+    ip = '175.114.21.96'
+    location = get_ip_location(ip)
+    
+    print(location)
