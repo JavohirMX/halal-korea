cc5917a1e0ec66097497117a1b640903f13d8921
JavohirMX <javohirmx@gmail.com>
2025-05-09 10:57:26 +0900

    Update db to PostgreSQL, add status to admin page, make places list fully clickable

--- config/settings.py
+++ config/settings.py
@@ -78,12 +78,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER': 'jmx',
+        'PASSWORD': 'jmx8520!',
+        'HOST': 'localhost',
+        'PORT': '5432',  # default PostgreSQL port
     }
 }
+
 
 
 # Password validation
