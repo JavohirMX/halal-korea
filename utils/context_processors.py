464aa94923aef768646eb79ee1a145d1414e9e63
JavohirMX <javohirmx@gmail.com>
2025-11-17 23:40:33 +0900

    fix: ensure all social media keys are present in context to prevent KeyError in templates

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -34,4 +34,11 @@ def static_info_context(request):
     # Add Google Maps ID
     info['google_maps_id'] = settings.GOOGLE_MAPS_ID
     
+    # Ensure all social media keys exist (even if None) to prevent KeyError in templates
+    # This allows templates to safely check if STATIC_INFO.facebook, etc. exist
+    social_keys = ['facebook', 'twitter', 'instagram']
+    for key in social_keys:
+        if key not in info:
+            info[key] = None
+    
     return {'STATIC_INFO': info}
